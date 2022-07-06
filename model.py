cde55c924c6ebb2fc04925879042a9f6359c1d66
Shankhanil Ghosh <shankha.rik@gmail.com>
2022-07-06 09:44:17 +0530

    reduced training parameters

--- model.py
+++ model.py
@@ -1,28 +1,3 @@
-import torch.nn as nn
-class ResBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, downsample):
-        super().__init__()
-        if downsample:
-            self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1)
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=2),
-                nn.BatchNorm2d(out_channels)
-            )
-        else:
-            self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
-            self.shortcut = nn.Sequential()
-
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-
-    def forward(self, input):
-        shortcut = self.shortcut(input)
-        input = nn.ReLU()(self.bn1(self.conv1(input)))
-        input = nn.ReLU()(self.bn2(self.conv2(input)))
-        input = input + shortcut
-        return nn.ReLU()(input)
-
 #@title
 class Net(nn.Module):
     def __init__(self, in_channels, resblock, outputs=1000):
@@ -40,20 +15,21 @@ class Net(nn.Module):
         )
 
         self.layer2 = nn.Sequential(
-            resblock(64, 128, downsample=True),
-            resblock(128, 128, downsample=False)
+            resblock(64, 256, downsample=True),
+            resblock(256, 256, downsample=False)
         )
 
         self.layer3 = nn.Sequential(
-            resblock(128, 256, downsample=True),
+            resblock(256, 256, downsample=True),
+
             resblock(256, 256, downsample=False)
         )
 
 
-        self.layer4 = nn.Sequential(
-            resblock(256, 512, downsample=True),
-            resblock(512, 512, downsample=False)
-        )
+        # self.layer4 = nn.Sequential(
+        #     resblock(256, 512, downsample=True),
+        #     resblock(512, 512, downsample=False)
+        # )
 
         self.gap = torch.nn.AdaptiveAvgPool2d(1)
         self.fc = torch.nn.Linear(1024, 10)
@@ -73,3 +49,13 @@ class Net(nn.Module):
 
         return input
   
+net = Net(3, ResBlock, outputs = 4)
+import torch.optim as optim
+
+from torchsummary import summary
+# summary(net, (3,32,32))
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+epoch = 30
+scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.01, steps_per_epoch=len(trainloader), epochs=epoch)
